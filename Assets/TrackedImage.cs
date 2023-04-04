db2c405a4004ab5d5a778cfbc329ce8e103b5fdc
garguiz21 <116018761+garguiz21@users.noreply.github.com>
2023-04-04 16:51:03 +0200

    Assets

---
+++ Assets/TrackedImage.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImage : MonoBehaviour
+{
+    private ARTrackedImageManager _trackedImageManager;
+
+    public GameObject currentPrefab;
+
+    private readonly Dictionary<string, GameObject> _instantiatedPrefabs = new Dictionary<string, GameObject>();
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        _trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        _trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach(var trackedImage in eventArgs.added) {
+            var imageName = trackedImage.referenceImage.name;
+                if(string.Compare(currentPrefab.name, imageName, System.StringComparison.OrdinalIgnoreCase) == 0 && !_instantiatedPrefabs.ContainsKey(imageName))
+                {
+                    var newPrefab = Instantiate(currentPrefab, trackedImage.transform);
+
+                    _instantiatedPrefabs[imageName] = newPrefab;
+                }
+            
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            Destroy(_instantiatedPrefabs[trackedImage.referenceImage.name]);
+            _instantiatedPrefabs.Remove(trackedImage.referenceImage.name);
+        }
+
+    }
+}
